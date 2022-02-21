935a2c5c0f47257d5c75509d90f6a6d02fd32b49
Joseph <qjlu@ucsd.edu>
2022-02-21 12:46:35 -0800

    added form when order and clear order when an order is submitted

--- 02-foodorder-project/src/components/Cart/Cart.js
+++ 02-foodorder-project/src/components/Cart/Cart.js
@@ -1,11 +1,14 @@
 import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
-import { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import CartItem from './CartItem';
+import Checkout from './Checkout'
 
 const Cart = props => {
-
+    const [isCheckout, setIsCheckout] = useState(false);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [didSubmit, setDidSubmit] = useState(false);
     const cartCtx = useContext(CartContext);
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
     const hasItems = cartCtx.items.length > 0;
@@ -31,18 +34,52 @@ const Cart = props => {
                       </ul>;
 
 
+    const orderHandler = () => {
+        setIsCheckout(true);
+    };
 
-    return (
-        <Modal onClose={props.onClose}>
-            {cartItems}
+    const submitOrderHandler = async (userData) => {
+        setIsSubmitting(true);
+        const response = await fetch('https://react-food-order-59b65-default-rtdb.firebaseio.com/orders.json', {
+            method: 'POST',
+            body: JSON.stringify({
+                user: userData,
+                orderedItems: cartCtx.items
+            })
+        });
+        setIsSubmitting(false);
+        setDidSubmit(true);
+        cartCtx.clearCart();
+    }
+
+    const modalActions = <div className={classes.actions}>
+                           <button className={classes['button--alt']}  onClick={props.onClose}>Close</button>
+                           {hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>}
+                        </div>
+
+    const cartModalContent = <React.Fragment>
+        {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
                 <span>{totalAmount}</span>
             </div>
-            <div className={classes.actions}>
-                <button className={classes['button--alt']}  onClick={props.onClose}>Close</button>
-                {hasItems && <button className={classes.button}>Order</button>}
-            </div>
+            {isCheckout && <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose}/>}
+            {!isCheckout && modalActions}
+    </React.Fragment>
+
+    const isSubmittingModalContent = <p>Sending order data...</p>
+  
+    const didSubmitModalContent = <React.Fragment>
+                                    <p>Successfully sent the order!</p>
+                                    <div className={classes.actions}>
+                                      <button className={classes.button} onClick={props.onClose}>Close</button>
+                                    </div>
+                                  </React.Fragment>
+    return (
+        <Modal onClose={props.onClose}>
+            {!isSubmitting && !didSubmit && cartModalContent}
+            {isSubmitting && isSubmittingModalContent}
+            {didSubmit && didSubmitModalContent}
         </Modal>
    );
 };
--- 02-foodorder-project/src/components/Meals/AvailableMeals.js
+++ 02-foodorder-project/src/components/Meals/AvailableMeals.js
@@ -1,37 +1,59 @@
 import classes from './AvailbleMeals.module.css';
+import { useEffect, useState} from 'react';
 import Card from '../UI/Card';
 import MealItem from './MealItem/Mealitem';
-
-const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
   
 const AvailbleMeals = () => {
+    const [meals, setMeals] = useState([]);
+    const [isLoading, setIsLoading] = useState(true); 
+    const [httpError, settHttpError] = useState(null);
 
-    const mealsList = DUMMY_MEALS.map(meal => <MealItem id={meal.id}
+    useEffect(() => {
+      const fetchMeals = async () => {
+        const response = await fetch('https://react-food-order-59b65-default-rtdb.firebaseio.com/meals.json')
+
+        if (!response.ok) {
+          throw new Error('Something went wrong!');
+        }
+
+        const responseData = await response.json();
+
+        const loadedMeals = [];
+
+        for (const key in responseData) {
+          loadedMeals.push({
+            id: key,
+            name: responseData[key].name,
+            description: responseData[key].description,
+            price: responseData[key].price
+          })
+        }
+
+        setMeals(loadedMeals);
+        setIsLoading(false);
+      }
+      
+        fetchMeals().catch(error => {
+          setIsLoading(false);
+          settHttpError(error.message);
+        });
+
+
+    }, [])
+
+    if (isLoading) {
+      return <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    }
+
+    if (httpError) {
+      return <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    }
+
+    const mealsList = meals.map(meal => <MealItem id={meal.id}
                                                key = {meal.id} 
                                                name={meal.name}
                                                description={meal.description}
