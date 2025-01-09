d3b60abfd5a50328aa0e6942ad84cd5d64768ec1
Joseph <qjlu@ucsd.edu>
2025-01-08 22:28:09 -0800

    add comment where i learned it from

--- 04-sse/server/src/index.ts
+++ 04-sse/server/src/index.ts
@@ -6,6 +6,7 @@ const PORT = 3000;
 
 app.use(cors());
 
+// learn from https://shaxadd.medium.com/effortless-real-time-updates-in-react-with-server-sent-events-a-step-by-step-guide-using-node-js-52ecff6d828e
 app.get("/events", (req, res) => {
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
@@ -14,7 +15,7 @@ app.get("/events", (req, res) => {
   const intervalId = setInterval(() => {
     const message = { data: "Hello from the server!" };
     res.write(`data: ${JSON.stringify(message)}\n\n`);
-  }, 5000);
+  }, 1000);
 
   req.on("close", () => {
     clearInterval(intervalId);
