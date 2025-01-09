f3e3b04243f90de8bd97bb440585af02d300217d
Joseph <qjlu@ucsd.edu>
2025-01-08 22:24:50 -0800

    feat(Toast): add UI for push notification

--- 04-sse/client/src/App.tsx
+++ 04-sse/client/src/App.tsx
@@ -1,12 +1,19 @@
+import { useEffect } from "react";
 import useSSE from "./useSSE";
+import { ToastContainer } from "react-toastify";
+import { toast } from "react-toastify";
 
 function App() {
   const { data, error } = useSSE("http://localhost:3000/events");
 
+  useEffect(() => {
+    toast(data?.data);
+  }, [data]);
   return (
     <div>
       <h1>Server-Sent Events in React</h1>
       {error && <p>{error}</p>}
+      <ToastContainer />
       {data ? (
         <p>Message from server: {data?.data}</p>
       ) : (
